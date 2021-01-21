6e2caa352b2571224c09f56152078bf34a05248a
PrashantYadav2304 <2000prashant23@gmail.com>
2021-01-21 22:07:13 +0530

    event activity added

--- app/src/main/java/com/printhub/printhub/eventActivity.java
+++ app/src/main/java/com/printhub/printhub/eventActivity.java
@@ -1,20 +1,14 @@
 package com.printhub.printhub;
 
-import android.os.Bundle;
-
-
-
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.os.Bundle;
 
 public class eventActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
     }
-
-
 }
